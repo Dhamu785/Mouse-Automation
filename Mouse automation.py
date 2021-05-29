52d2303cac8a4497bd59e1a79acbe4ec92517d5c
Dhamu785 <76835665+Dhamu785@users.noreply.github.com>
2021-05-29 14:21:02 +0530

    Create Mouse automation.py

---
+++ Mouse automation.py
@@ -0,0 +1,18 @@
+import pyautogui
+import webbrowser
+pyautogui.confirm('Make sure that your screen size is "1080 x 1990"')
+link = input('Enter the you tube channel link : ')
+webbrowser.open_new_tab(link)
+pyautogui.sleep(5)
+pyautogui.press('F11')
+pyautogui.sleep(1)
+pyautogui.moveTo(x= 1650, y= 376,duration=1 )
+pyautogui.leftClick(x=1650, y= 376)
+pyautogui.moveTo(x=1692, y= 382,duration=1 )
+
+pyautogui.leftClick(x=1692, y= 382)
+pyautogui.moveTo(x=1735, y= 424,duration=1 )
+
+pyautogui.leftClick(x=1735, y= 424)
+pyautogui.press('F11')
+pyautogui.confirm('Succesfully subscribed..!!')
